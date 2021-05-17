9cba352b432394be9c61e39bcadce011f2e3b10c
670-qliu <jerry197352468@gmail.com>
2021-05-17 11:48:26 -0700

    add like button

--- client/src/components/Goal/Goal.js
+++ client/src/components/Goal/Goal.js
@@ -2,7 +2,31 @@ import React from 'react';
 import{useState} from 'react'
 import "./Goal.css"
 
-
+class LikeButton extends React.Component {
+    constructor() {
+      super();
+      this.state = {
+        liked: false
+      };
+      this.handleClick = this.handleClick.bind(this);
+    } 
+    
+    handleClick() {
+      this.setState({
+        liked: !this.state.liked
+      });
+    }
+    
+    render() {
+      const label = this.state.liked ? 'Unlike' : 'Like'
+      return (
+        <div className="customContainer">
+          <button className="btn btn-primary" onClick={this.handleClick}>
+            {label}</button>
+        </div>
+      );
+    }
+  }
 
 
 function Goal(props){
@@ -18,11 +42,9 @@ function Goal(props){
                     {/* {props.likesCount+" "} */}
                     100
                 </div>
-                <div className="icons">
-                <i class="far fa-heart"></i>
-                    
-                    
-                </div>
+
+                <LikeButton />
+
                 <a href="https://www.youtube.com/">
                     Comments
                     {/* todo: need to add return in the jump page */}
