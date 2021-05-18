c4a55ce45e88e7538967c4ac21f69a29c1fcb90c
Zihan Xue <zihan.x@foxmail.com>
2021-05-17 18:02:31 -0700

    Add fake data

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 
 import Nav from './components/Nav/Nav';
 import Goal from './components/Goal/Goal';
 
-function App(props) {
+const data = [
+  {
+    "id": 1,
+    "name": "Learn React",
+    "desc": "React is fun",
+  },
+  {
+    "id": 2,
+    "name": "Learn Node",
+    "desc": "Node is fun",
+  },
+  {
+    "id": 3,
+    "name": "Learn Express",
+    "desc": "Express is fun",
+  },
+  {
+    "id": 4,
+    "name": "Learn MongoDB",
+    "desc": "MongoDB is fun",
+  },
+  {
+    "id": 5,
+    "name": "Learn HTML",
+    "desc": "HTML is fun",
+  },
+  {
+    "id": 6,
+    "name": "Learn CSS",
+    "desc": "CSS is fun",
+  },
+  {
+    "id": 7,
+    "name": "Learn HTTP",
+    "desc": "HTTP is fun",
+  },
+  {
+    "id": 8,
+    "name": "Learn async",
+    "desc": "Async is fun",
+  },
+  {
+    "id": 9,
+    "name": "Learn FP",
+    "desc": "FP is fun",
+  },
+]
+
+function App() {
+  const [goals, setGoals] = useState(data);
+
   return (
     <>
       <div className="App">
         <Nav />
         <div className="goals">
-          {[...Array(20)].map(() => <Goal name={"Learn React"} />)}
+          {goals.map((goal) => <Goal key={goal.id} name={goal.name} desc={goal.desc} />)}
         </div>
       </div>
     </>
