b6dee8578904f06e66a609e625cb5ca015c7e281
Bryan(Yu) Zhou <bryanzhou008@gmail.com>
2021-05-22 20:49:04 +0800

    added user schema, added some goal methods

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const connection = require('./models/connect.js')
 const goal_router = require('./routes/goal.js')
+const user_router = require('./routes/user.js')
 
 //All request start with "/goal" will be handled by goal_router
 app.use('/goal', goal_router)
+app.use('/user', user_router)
 
 app.get('/', (req, res) => {
     res.send("Helloworld")
