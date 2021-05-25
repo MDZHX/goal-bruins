d44ca39d0fcdd107751f33f26f3de761d8bc69ca
Qi Xiao <mattxiao2000@gmail.com>
2021-05-25 09:53:28 +0800

    Add user register and user login in server

--- server/routes/user.js
+++ server/routes/user.js
@@ -2,14 +2,48 @@ const express = require('express');
 const router = express.Router()
 const connection = require('../models/connect.js')
 const User = require('../models/user')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
-router.get('/add-goal', (req, res) =>{
-    const goal = new Goal({
-        name: 'study',
-        description: 'go study'
+//Parse the json object
+router.use(express.json())
+
+router.post("/login", async (req, res) =>{
+    const { username, password } = req.body;
+    let user;
+    await User.findOne({ username : `${username}` }, (err, user_back) =>{
+        if (err){
+            return res.json({ status: 'error', error: 'Invalid username' })
+        }
+
+        
+        user = user_back;
     })
 
-    goal.save()
+    if (password == user.password){
+        return res.json({
+            status: 'ok'
+        })
+    }
+
+    res.json({
+        status: 'error',
+        error: 'Invalid password'
+    })
+})
+
+
+
+router.post('/register', (req, res) =>{
+
+    const { username, password: plainTextPassword } = req.body
+
+    const user = new User({
+        username: `${username}`,
+        password: `${plainTextPassword}`
+    })
+
+    user.save()
         .then((result) => {
             res.send(result)
         })
